492e4950d9f55c630090dd08d4191e706fd10b2a
AbdelazimAhmed <abdelzeem756@gmail.com>
2022-11-06 09:31:19 +0200

    STM_haderFile_fix_err

--- scheduler_st/Inc/stm32f03xx.h
+++ scheduler_st/Inc/stm32f03xx.h
@@ -12,26 +12,24 @@
 #include "STD_TYPE.h"
 
 #define SARM_SIZE			((8)*(1024))
-#define SRAM_BASEADD		(0x20000000U)
+#define SRAM_BASEADD			(0x20000000U)
 #define SRAM_ENDADD			(0x20001FFFU)   //  SRAM_BASEADD (HEX) + ((8*1024)-1) HEX
 
 
 /* RCC REGISTERS */
-#define RCC_CR				*((__vo u32 *)0x40021000)
-#define RCC_CFGR			*((__vo u32 *)0x40021004)
-#define RCC_AHBENR			*((__vo u32 *)0x40021014)
+#define RCC_CR				(*(__vo u32 *)0x40021000)
+#define RCC_CFGR			(*(__vo u32 *)0x40021004)
+#define RCC_AHBENR			(*(__vo u32 *)0x40021014)
 
 /* systick timer */
-#define SYST_CSR			*((__vo u32 *)0xE000E010)
-#define SYST_RVR			*((__vo u32 *)0xE000E014)
+#define SYST_CSR			(*(__vo u32 *)0xE000E010)
+#define SYST_RVR			(*(__vo u32 *)0xE000E014)
 
-#define GPIOA_MODER			*((__vo u32 *)0x48000000)
-#define GPIOx_ODR			*((__vo u32 *)0x48000014)
+#define GPIOA_MODER			(*(__vo u32 *)0x48000000)
+#define GPIOx_ODR			(*(__vo u32 *)0x48000014)
 
 
 /* Interrupt Control and State Register */
-#define ICSR 				*((__vo u32 *)0xE000ED04)
+#define ICSR 				(*(__vo u32 *)0xE000ED04)
 
-
-//							*((__vo u32 *)0xE000E010)
 #endif /* DEVICE_HEADER_H_ */
